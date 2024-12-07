31d939c76464f3679c63c1dee5c3704e006c836f
Samara Monteiro Xavier <samaraxavier336@gmail.com>
2024-12-07 18:34:27 -0300

    Alteracao nas classes Pessoa e Administrados

--- projeto/src/timeShare/Administrador.java
+++ projeto/src/timeShare/Administrador.java
@@ -1,40 +1,40 @@
 package timeShare;
 
 import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class Administrador extends Pessoa {
 
-	public Administrador(String nome, String cpf, String email, String senha) {
-		super(nome, cpf, email, senha);
-	}
+    private ArrayList<Bem> bens;
 
-	private ArrayList<Bem> bensCadastrados;
+    public Administrador(String nome, String cpf, String email, String senha, LocalDate dataNasicmento) {
+        super(nome, cpf, email, senha, dataNascimento);
+        this.bens = new ArrayList<Bem>();
+    }
 
-	public void cadastrarBem(String nome, String descricao, String localizacao, int capacidade) {
-		if (bensCadastrados == null) {
-			bensCadastrados = new ArrayList<>();
-		}
-		Bem novo = new Bem(nome, descricao, localizacao, capacidade);
-		bensCadastrados.add(novo);
-	}
+    public void cadastrarBem(String nome, String descricao, String localizacao, int capacidade) {
+        Bem novo = new Bem(nome, descricao, localizacao, capacidade);
+        this.bens.add(novo);
+    }
 
-	public void definirPrecoFracao(Bem bem, int fracaoId, double preco) {
-		if (bem.getFracoes() == null) {
-			System.out.println("Nao ha fracoes para o bem.");
-		}
-		Fracao fracao = bem.getFracao(fracaoId);
-		if (fracao != null) {
-			fracao.setPreco(preco);
-			System.out.println("Preco definido");
-		} else {
-			System.out.println("Erro");
-		}
-	}
-	
-	public void gerenciarFracoes(int bemId) {
+    public void removerBem(Bem bem) {
+        this.bens.remove(bem);
+    }
 
-	}
-   public consultarRelatorios() {
-	
-}
+    public void gerenciarCotas(int bemId) {
+
+    }
+
+    public consultarRelatorios() {
+
+    }
+
+    public ArrayList<Bem> getBens(int id) {
+        for(Bem b : bens) {
+            if(b.getId() == id) {
+
+            }
+        };
+    }
+
 }
