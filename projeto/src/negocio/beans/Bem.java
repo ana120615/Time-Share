519d6efdad8e01c9490874030bafd668b2844754
lailasam <samaralailaa@gmail.com>
2025-01-07 11:52:22 -0300

    Camadas
    Adição de negocio, exceções e dados

---
+++ projeto/src/negocio/beans/Bem.java
@@ -0,0 +1,50 @@
+package negocio.beans;
+
+public class Bem {
+    private int id;
+    private String nome;
+    private String descricao;
+    private String localizacao;
+    private int capacidade;
+
+    //METODOS GET E SET
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getLocalizacao() {
+        return this.localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public int getCapacidade() {
+        return this.capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+}
---
+++ projeto/src/negocio/beans/Usuario.java
@@ -0,0 +1,55 @@
+package negocio.beans;
+
+import java.time.LocalDate;
+
+public abstract class Usuario {
+
+    private String cpf;
+    private String nome;
+    private String email;
+    private String senha;
+    private LocalDate dataNascimento;
+
+    //METODOS GET E SET
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    //METODO ABSTRATO
+public abstract boolean eAdm();
+}
