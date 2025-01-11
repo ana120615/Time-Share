5dd46cff51814759ede623d3ff2a56c0eeb9e938
Cauã Teodoro <cauawallacygt@gmail.com>
2025-01-11 19:53:42 -0300

    Atualização pequena em cota

--- projeto/src/br/ufrpe/time_share/Programa.java
+++ projeto/src/br/ufrpe/time_share/Programa.java
@@ -10,66 +10,39 @@ import java.util.ArrayList;
 
 public class Programa {
     public static void main(String[] args) {
+        // USUÀRIO ADM
+        Usuario adm = new UsuarioAdm(111222336, "$$767$$");
+        // BEM
+        Bem bem = new Bem(1111, "Apartamento beira-mar", "Centro do Recife",
+                "Recife-Pe", 6);
+        // COTAS
+        Cota c1 = new Cota(6241, LocalDate.now(), LocalDate.of(2025, 1, 20),
+                100000);
 
-        RepositorioBens repositorioBens = new RepositorioBens();
+        Cota c2 = new Cota(9542, LocalDate.of(2025, 2, 1), LocalDate.of(2025, 2, 10), 500000);
 
-        Usuario adm1 = new UsuarioAdm(1223548, "senhaAdm1");
+        // CADASTRANDO UMA COTA
+        ((UsuarioAdm) adm).cadastrarBem(bem);
 
-        Bem bem = new Bem(1111, "Casa na Praia", "Belas vistas",
-                "Recife-Pe", 4);
-        Bem bem2 = new Bem(2222, "Apartamento", "Centro",
-                "Recife-Pe", 2);
-        ((UsuarioAdm) adm1).cadastrarBem(bem);
-        ((UsuarioAdm) adm1).cadastrarBem(bem2);
+        // ADICIONANDO UMA COTA NO BEM
+        bem.adicionarCota(c1);
+        bem.adicionarCota(c2);
 
-        Cota cota1 = new Cota(1234, LocalDate.now(), 950.50);
-        Cota cota2 = new Cota(2617, LocalDate.now(), 600);
+        // USUARIO COMUM
+        Usuario userComum = new UsuarioComum(8845412, "LALA%$");
 
+        Venda v1 = new Venda(6666, (UsuarioComum) userComum);
 
-        bem.adicionarCota(cota1);
-        bem.adicionarCota(cota2);
+        v1.adicionarCotaCarrinho(c1);
+        v1.adicionarCotaCarrinho(c2);
 
-        repositorioBens.cadastrarBem(bem);
-        repositorioBens.cadastrarBem(bem2);
-
-        System.out.println(repositorioBens.buscarBemPorId(111));
-
-        System.out.println("\n\n");
-        ArrayList<Bem> bensNoRepo = repositorioBens.listarBensDisponiveis();
-        for (Bem b : bensNoRepo) {
-            System.out.println(b);
-        }
-        System.out.println("\n\n");
-
-
-        //System.out.println(((UsuarioAdm) adm1).consultarBens());
-
-        //System.out.println(bem.getCotas());
-
-        UsuarioComum comum = new UsuarioComum(153284, "545415@@Senha");
-
-        Venda v1 = new Venda(1512, comum);
-
-        v1.adicionarCotaCarrinho(cota1);
-        v1.adicionarCotaCarrinho(cota2);
-
-        //System.out.println(v1.gerarNotaFiscal());
-
-        // Em tese não finalizou a compra
-        System.out.println(comum.getCotasAdquiridas());
+        System.out.println(v1);
 
         v1.finalizarCompra();
-        Cota cota3 = new Cota(5555, LocalDate.now(), 400.50);
-        Cota cota4 = new Cota(6666, LocalDate.now(), 600);
-        Venda v2 = new Venda(151, comum);
-        v2.adicionarCotaCarrinho(cota3);
-        v2.adicionarCotaCarrinho(cota4);
-        System.out.println(comum.getCotasAdquiridas());
 
-        v2.finalizarCompra();
+        // VERIFICANDO SE USUARIO COMUM POSSUI AS COTAS DEPOIS DE FINALIZAR A COMPRA
+        System.out.println(((UsuarioComum) userComum).getCotasAdquiridas());
 
-        // Agora posso acessar as cotas compradas no Usuário Comum
-        System.out.println(comum.getCotasAdquiridas());
 
 
 
