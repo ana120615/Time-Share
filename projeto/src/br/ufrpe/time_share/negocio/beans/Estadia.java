416365b783c2a4bbfb213f34f18435df32492ea0
Cauã Teodoro <cauawallacygt@gmail.com>
2025-01-11 22:20:09 -0300

    att

--- projeto/src/br/ufrpe/time_share/negocio/beans/Estadia.java
+++ projeto/src/br/ufrpe/time_share/negocio/beans/Estadia.java
@@ -1,14 +1,34 @@
 package br.ufrpe.time_share.negocio.beans;
 
+import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class Estadia {
     private int id;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
     private Reserva reserva;
 
     public Estadia(int id, Reserva reserva) {
         this.reserva = reserva;
         this.id = id;
+        setDataInicio(reserva.getDataInicio());
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
     }
 
     public int getId() {
@@ -30,5 +50,15 @@ public class Estadia {
     //CALCULAR DURACAO DA ESTADIA
     public int calcularDuracao () {
       return (int) ChronoUnit.DAYS.between (reserva.getDataInicio(),reserva.getDataFim());
+    }
+
+    @Override
+    public String toString() {
+        return "Estadia{" +
+                "id=" + id +
+                ", dataInicio=" + dataInicio +
+                ", dataFim=" + dataFim +
+                ", reserva=" + reserva +
+                '}';
     }
 }
--- projeto/src/br/ufrpe/time_share/negocio/beans/Reserva.java
+++ projeto/src/br/ufrpe/time_share/negocio/beans/Reserva.java
@@ -1,51 +1,26 @@
 package br.ufrpe.time_share.negocio.beans;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public class Reserva {
     private int id;
     private LocalDate dataInicio;
     private LocalDate dataFim;
     private boolean status;
-    private double taxa;
-    private Usuario usuario;
     private UsuarioComum usuarioComum;
-    private UsuarioAdm usuarioAdm;
     private Bem bem;
-    private List<Cota> cotas;
 
-    public Reserva(int id, LocalDate dataInicio, LocalDate dataFim, Usuario usuario, Bem bem, List<Cota> cotas) {
+    public Reserva(int id, LocalDate dataInicio, LocalDate dataFim, UsuarioComum usuarioComum, Bem bem) {
         this.id = id;
         this.status = true;
         this.dataInicio = dataInicio;
         this.dataFim = dataFim;
-        this.taxa = 0.00d;
-        this.usuario = usuario;
+        this.usuarioComum = usuarioComum;
         this.bem = bem;
-        this.cotas = cotas;
     }
-
-  
 
     public void cancelarReserva() {
         this.status = false;
-    }
-
-    public boolean validarReserva(){
-            for (Cota cota: cotas){
-                if(!cota.isStatusDeDisponibilidadeParaReserva()){
-                    // para ajudar no debug 
-                    System.out.println("cota id" + cota.getId()+ "não está disponível para comprar");
-                    return false;
-                }
-               
-            } 
-            //debug
-            System.out.println("Períodos e cotas estão disponíveis e válidos!");
-            return true;
-       
-    
     }
 
     public Bem getBem() {
@@ -54,14 +29,6 @@ public class Reserva {
 
     public void setBem(Bem bem) {
         this.bem = bem;
-    }
-
-    public double getTaxa() {
-        return taxa;
-    }
-
-    public void setTaxa(double taxa) {
-        this.taxa = taxa;
     }
 
     public void setStatus(boolean status) {
@@ -96,38 +63,38 @@ public class Reserva {
         this.id = id;
     }
 
-    public Usuario getUsuario() {
-        return usuario;
-    }
 
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-    public List<Cota> getCotas() {
-        return cotas;
-    }
-
-    public void setCotas(List<Cota> cotas) {
-        this.cotas = cotas;
-  
-    }
     public UsuarioComum getUsuarioComum() {
         return usuarioComum;
     }
 
 
-
     public void setUsuarioComum(UsuarioComum usuarioComum) {
         this.usuarioComum = usuarioComum;
     }
-    public UsuarioAdm getUsuarioAdm() {
-        return usuarioAdm;
+
+    @Override
+    public String toString() {
+        return "Reserva{" +
+                "id=" + id +
+                ", dataInicio=" + dataInicio +
+                ", dataFim=" + dataFim +
+                ", status=" + status +
+                ", usuarioComum=" + usuarioComum +
+                ", bem=" + bem +
+                '}';
     }
 
+    @Override
+    public boolean equals(Object object) {
+        boolean resultado = false;
+        if (object instanceof Reserva) {
+            Reserva reserva = (Reserva) object;
+            resultado = this.id == reserva.id && this.dataInicio.equals(reserva.dataInicio)
+                    && this.dataFim.equals(reserva.dataFim);
+        }
 
-
-    public void setUsuarioAdm(UsuarioAdm usuarioAdm) {
-        this.usuarioAdm = usuarioAdm;
+        return resultado;
     }
 
 
