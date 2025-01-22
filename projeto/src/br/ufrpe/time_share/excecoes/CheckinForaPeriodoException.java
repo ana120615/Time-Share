7980552937520b76bf55c4cdc52f7e9ca21e00e1
lailasam <samaralailaa@gmail.com>
2025-01-22 14:30:41 -0300

    alteracao em criar reserva

--- projeto/src/br/ufrpe/time_share/excecoes/CheckinForaPeriodoException.java
+++
@@ -1,7 +0,0 @@
-package br.ufrpe.time_share.excecoes;
-
-public class CheckinForaPeriodoException extends Exception {
-    public CheckinForaPeriodoException(String message){
-    super(message);
-    }
-}
