1357297821a040eba6f63bd314e5bc22c7027f12
Cauã Teodoro <cauawallacygt@gmail.com>
2025-01-22 15:25:18 -0300

    atualizando enum

--- projeto/src/br/ufrpe/time_share/Programa.java
+++ projeto/src/br/ufrpe/time_share/Programa.java
@@ -8,26 +8,28 @@ import br.ufrpe.time_share.negocio.beans.*;
 public class Programa {
     public static void main(String[] args) {
 
-        Usuario usuario = new Usuario("132644541", "Senha123", TipoUsuario.ADMINISTRADOR);
+        Usuario usuario = new Usuario("132644541", "Senha1", TipoUsuario.COMUM);
         Usuario usuario2 = new Usuario("65156155", "Senha9542", TipoUsuario.COMUM);
         Usuario usuario3 = new Usuario("45165206", "S@NHASUF", TipoUsuario.COMUM);
         Usuario usuario4 = new Usuario("511006515", "#afjnja", TipoUsuario.ADMINISTRADOR);
         Usuario usuario5 = new Usuario("471652206", "fafeof", TipoUsuario.ADMINISTRADOR);
 
+        if (usuario.getTipo().VALOR == 1) {
+            System.out.println("Adm");
+        }
 
-
-        IRepositorioUsuario repo = new RepositorioUsuarios();
-        repo.cadastrar(usuario);
-        repo.cadastrar(usuario2);
-        repo.cadastrar(usuario3);
-        repo.cadastrar(usuario4);
-        repo.cadastrar(usuario5);
-
-        System.out.println("LISTANDO USUÁRIOS COMUMS");
-        System.out.println(repo.listarUsuarioComum());
-
-        System.out.println("\n\nLISTANDO USUÁROS ADMINISTRADORES");
-        System.out.println(repo.listarUsuarioAdm());
+//        IRepositorioUsuario repo = new RepositorioUsuarios();
+//        repo.cadastrar(usuario);
+//        repo.cadastrar(usuario2);
+//        repo.cadastrar(usuario3);
+//        repo.cadastrar(usuario4);
+//        repo.cadastrar(usuario5);
+//
+//        System.out.println("LISTANDO USUÁRIOS COMUMS");
+//        System.out.println(repo.listarUsuarioComum());
+//
+//        System.out.println("\n\nLISTANDO USUÁROS ADMINISTRADORES");
+//        System.out.println(repo.listarUsuarioAdm());
 
     }
 }
--- projeto/src/br/ufrpe/time_share/negocio/beans/TipoUsuario.java
+++ projeto/src/br/ufrpe/time_share/negocio/beans/TipoUsuario.java
@@ -1,6 +1,14 @@
 package br.ufrpe.time_share.negocio.beans;
 
 public enum TipoUsuario {
-    ADMINISTRADOR,
-    COMUM
+    ADMINISTRADOR (1),
+    COMUM (2);
+
+    public final int VALOR;
+
+    TipoUsuario(int valor) {
+        this.VALOR = valor;
+    }
+
+
 }
