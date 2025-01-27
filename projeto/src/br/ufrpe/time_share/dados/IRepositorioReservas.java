a5beec6b5255004829a109ac4bc1f7c69dcf30ec
Cauã Teodoro <cauawallacygt@gmail.com>
2025-01-27 19:33:33 -0300

    att estadia e reserva

--- projeto/src/br/ufrpe/time_share/dados/IRepositorioReservas.java
+++ projeto/src/br/ufrpe/time_share/dados/IRepositorioReservas.java
@@ -3,14 +3,15 @@ package br.ufrpe.time_share.dados;
 import br.ufrpe.time_share.negocio.beans.Reserva;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface IRepositorioReservas extends IRepositorio<Reserva> {
 
     Reserva buscarReserva(Reserva reserva);
 
-    ArrayList<Reserva> buscarReservasPorUsuario(int idUsuario);
+    List<Reserva> buscarReservasPorUsuario(int idUsuario);
 
-    ArrayList<Reserva> buscarReservasPorBem(int idBem);
+    List<Reserva> buscarReservasPorBem(int idBem);
 
 
 }
