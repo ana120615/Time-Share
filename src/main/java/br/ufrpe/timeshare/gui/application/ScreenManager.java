f5775ac549b85bfffec03d608090976b27e4449b
lailasam <samaralailaa@gmail.com>
2025-02-16 19:33:32 -0300

    update

--- src/main/java/br/ufrpe/timeshare/gui/application/ScreenManager.java
+++ src/main/java/br/ufrpe/timeshare/gui/application/ScreenManager.java
@@ -41,6 +41,7 @@ public class ScreenManager {
         carregarTela("TelaDeVendas", "/br/ufrpe/timeshare/gui/application/TelaDeVenda.fxml");
         carregarTela("TelaDeCotas", "/br/ufrpe/timeshare/gui/application/TelaDeCotas.fxml");
         carregarTela("TelaMinhasCotas", "/br/ufrpe/timeshare/gui/application/TelaMinhasCotas.fxml");
+        carregarTela("TelaReservasComum", "/br/ufrpe/timeshare/gui/application/BuscarBensReserva.fxml");
     }
 
     private void carregarTela(String nome, String caminhoFXML) {
@@ -83,6 +84,10 @@ public class ScreenManager {
                 }
             }
         }
+    }
+
+    public void showReservaComumScreen(){
+    showScreen("TelaReservasComum");
     }
 
     public void showCadastroScreen() {
--- src/main/java/br/ufrpe/timeshare/gui/controllers/usuarioComum/telaReservas/ControllerReservasComum.java
+++ src/main/java/br/ufrpe/timeshare/gui/controllers/usuarioComum/telaReservas/ControllerReservasComum.java
@@ -1,5 +1,16 @@
 package br.ufrpe.timeshare.gui.controllers.usuarioComum.telaReservas;
 
+
+
 public class ControllerReservasComum {
     
+    private ControladorReservas controladorReservas;
+    {
+        controladorReservas = new ControladorReservas();
+    }
+
+    @FXML
+    private void buscarBensDisponiveis(ActionEvent event){
+    
+    }
 }
