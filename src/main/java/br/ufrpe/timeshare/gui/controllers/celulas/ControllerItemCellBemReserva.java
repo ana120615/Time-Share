8d34ccc5957ab99c87f1def388198506583e0c66
lailasam <samaralailaa@gmail.com>
2025-02-18 19:32:55 -0300

    add verificar disponibilidade no datepicker

--- src/main/java/br/ufrpe/timeshare/gui/controllers/celulas/ControllerItemCellBemReserva.java
+++ src/main/java/br/ufrpe/timeshare/gui/controllers/celulas/ControllerItemCellBemReserva.java
@@ -1,233 +1,478 @@
 package br.ufrpe.timeshare.gui.controllers.celulas;
 
+
+
 import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
+
 import javafx.geometry.Insets;
+
 import javafx.scene.Scene;
+
 import javafx.scene.control.Alert;
+
 import javafx.scene.control.Button;
+
 import javafx.scene.control.DateCell;
+
 import javafx.scene.control.DatePicker;
+
 import javafx.scene.control.Label;
+
 import javafx.scene.control.TextField;
+
 import javafx.scene.image.Image;
+
 import javafx.scene.image.ImageView;
+
 import javafx.scene.layout.HBox;
+
 import javafx.scene.layout.VBox;
+
 import javafx.stage.Modality;
+
 import javafx.stage.Stage;
+
 import br.ufrpe.timeshare.gui.application.ScreenManager;
+
 import br.ufrpe.timeshare.negocio.ControladorReservas;
+
 import br.ufrpe.timeshare.negocio.beans.Bem;
+
 import br.ufrpe.timeshare.negocio.beans.Usuario;
 
+
+
 import java.io.File;
+
 import java.time.LocalDate;
+
 import java.time.LocalDateTime;
+
 import java.time.format.DateTimeFormatter;
+
 import java.util.List;
+
 import java.util.stream.Collectors;
+
+
 
 public class ControllerItemCellBemReserva {
 
-    @FXML
-    private HBox itemCell;
 
-    @FXML
-    private ImageView imagemBem;
+@FXML
 
-    @FXML
-    private Label nomeBem;
-
-    @FXML
-    private Label descricaoBem;
-
-    @FXML
-    private Label localizacaoBem;
-
-    @FXML
-    private Label capacidadeBem;
-
-    private Bem bem;
-    private ControladorReservas controladorReservas;
-    private Usuario usuarioLogado;
-    private DatePicker dataInicioPicker;
-    private DatePicker dataFimPicker;
-
-    public void initialize() {
-        this.controladorReservas = new ControladorReservas();
-
-        // Usuario logado
-        Object data = ScreenManager.getInstance().getData();
-        if (data instanceof Usuario) {
-            this.usuarioLogado = (Usuario) data;
-        }
-
-        dataInicioPicker = new DatePicker();
-        dataFimPicker = new DatePicker();
-    }
+ private HBox itemCell;
 
 
-    public void setItem(Bem bem) {
-        if (bem != null) {
-            nomeBem.setText(bem.getNome());
-            descricaoBem.setText(bem.getDescricao());
-            localizacaoBem.setText(bem.getLocalizacao());
-            capacidadeBem.setText(String.valueOf(bem.getCapacidade()));
 
-            
-            if (bem.getCaminhoImagem() != null && !bem.getCaminhoImagem().isEmpty()) {
-                File file = new File(bem.getCaminhoImagem());
-                if (file.exists()) {
-                    Image image = new Image(file.toURI().toString());
-                    imagemBem.setImage(image);
+ @FXML
+
+private ImageView imagemBem;
+
+
+
+ @FXML
+
+ private Label nomeBem;
+
+
+
+ @FXML
+
+private Label descricaoBem;
+
+
+
+ @FXML
+
+private Label localizacaoBem;
+
+
+
+ @FXML
+
+ private Label capacidadeBem;
+
+
+
+ private Bem bem;
+
+ private ControladorReservas controladorReservas;
+
+private Usuario usuarioLogado;
+
+ private DatePicker dataInicioPicker;
+
+private DatePicker dataFimPicker;
+
+
+
+public void initialize() {
+
+ this.controladorReservas = new ControladorReservas();
+
+
+
+ // Usuario logado
+
+Object data = ScreenManager.getInstance().getData();
+
+if (data instanceof Usuario) {
+
+ this.usuarioLogado = (Usuario) data;
+
+}
+ this.dataInicioPicker = new DatePicker();
+
+ this.dataFimPicker = new DatePicker();
+
+}
+
+
+
+private void configurarDayCellFactory() {
+    if(dataInicioPicker!=null && dataFimPicker!=null){
+    try {
+        List<String> periodosDisponiveisString = controladorReservas.consultarDisponibilidadeParaReserva(bem, LocalDate.now().atStartOfDay(), LocalDate.now().plusYears(1).atStartOfDay(), usuarioLogado);
+        List<LocalDate> datasDisponiveis = periodosDisponiveisString.stream()
+                .map(dateString -> LocalDateTime.parse(dateString, DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                .map(LocalDateTime::toLocalDate)
+                .collect(Collectors.toList());
+
+        dataInicioPicker.setDayCellFactory(picker -> new DateCell() {
+            public void updateItem(LocalDate date, boolean empty) {
+                super.updateItem(date, empty);
+                if (empty || date == null || !datasDisponiveis.contains(date)) {
+                    setDisable(true);
+                    setStyle("-fx-background-color: #cccccc;");
                 } else {
-                    imagemBem.setImage(null); 
+                    setStyle("-fx-background-color: #ccffcc;");
                 }
-            } else {
-                imagemBem.setImage(null); 
             }
-        }
+        });
+
+        dataFimPicker.setDayCellFactory(picker -> new DateCell() {
+            public void updateItem(LocalDate date, boolean empty) {
+                super.updateItem(date, empty);
+                if (empty || date == null || !datasDisponiveis.contains(date)) {
+                    setDisable(true);
+                    setStyle("-fx-background-color: #cccccc;");
+                } else {
+                    setStyle("-fx-background-color: #ccffcc;");
+                }
+            }
+        });
+    } catch (Exception e) {
+        exibirAlertaErro("Erro", "Erro ao carregar datas disponíveis", e.getMessage());
     }
-    
+}
+else{
+    exibirAlertaErro("Erro", "Erro ao inicializar date pickers", "Os date pickers não foram inicializados corretamente");
+}
+}
 
-  @FXML
+
+
+public void setItem(Bem bem) {
+
+ if (bem != null) {
+
+    this.bem = bem;
+nomeBem.setText(bem.getNome());
+
+ descricaoBem.setText(bem.getDescricao());
+
+ localizacaoBem.setText(bem.getLocalizacao());
+
+ capacidadeBem.setText(String.valueOf(bem.getCapacidade()));
+
+
+
+if (bem.getCaminhoImagem() != null && !bem.getCaminhoImagem().isEmpty()) {
+
+ File file = new File(bem.getCaminhoImagem());
+
+ if (file.exists()) {
+
+ Image image = new Image(file.toURI().toString());
+
+imagemBem.setImage(image);
+
+ } else {
+
+ imagemBem.setImage(null);
+
+}
+
+ } else {
+
+ imagemBem.setImage(null);
+
+ }
+
+ }
+
+ }
+
+
+
+
+
+ @FXML
+
 public void abrirMiniTelaReserva(ActionEvent event) {
-    Stage popupStage = new Stage();
-    popupStage.initModality(Modality.APPLICATION_MODAL);
-    popupStage.setTitle("Reservar " + bem.getNome());
 
-    TextField mesesTextField = new TextField();
-    mesesTextField.setPromptText("Número de meses");
-    
-    DatePicker dataInicioPicker = new DatePicker();
-    DatePicker dataFimPicker = new DatePicker();
+Stage popupStage = new Stage();
 
-    Button confirmarButton = new Button("Confirmar");
-    Button cancelarButton = new Button("Cancelar");
+ popupStage.initModality(Modality.APPLICATION_MODAL);
 
-    confirmarButton.setOnAction(e -> {
-        String mesesStr = mesesTextField.getText();
-        LocalDate dataInicio = dataInicioPicker.getValue();
-        LocalDate dataFim = dataFimPicker.getValue();
+ popupStage.setTitle("Reservar " + bem.getNome());
 
-        if (!mesesStr.isEmpty() && dataInicio != null && dataFim != null) {
-            try {
-                int meses = Integer.parseInt(mesesStr);
-                confirmarReserva(event, dataInicio, dataFim, meses);
-                popupStage.close(); // Fechar a mini tela ao confirmar
-            } catch (NumberFormatException ex) {
-                exibirAlertaErro("Erro", "Período inválido", "Por favor, insira um número válido de meses.");
-            }
-        } else {
-            exibirAlertaErro("Erro", "Dados inválidos", "Por favor, preencha todos os campos.");
-        }
-    });
 
-    cancelarButton.setOnAction(e -> popupStage.close()); 
 
-    
-    VBox layout = new VBox(10);
-    layout.getChildren().addAll(mesesTextField, dataInicioPicker, dataFimPicker, confirmarButton, cancelarButton);
-    layout.setPadding(new Insets(20));
+TextField mesesTextField = new TextField();
 
-    
-    Scene scene = new Scene(layout);
-    popupStage.setScene(scene);
-    popupStage.showAndWait(); 
+ mesesTextField.setPromptText("Número de meses");
+
+
+
+Button confirmarButton = new Button("Confirmar");
+
+ Button cancelarButton = new Button("Cancelar");
+
+configurarDayCellFactory();
+
+ confirmarButton.setOnAction(e -> {
+
+String mesesStr = mesesTextField.getText();
+
+LocalDate dataInicio = dataInicioPicker.getValue();
+
+ LocalDate dataFim = dataFimPicker.getValue();
+
+
+
+if (!mesesStr.isEmpty() && dataInicio != null && dataFim != null) {
+
+ try {
+
+ int meses = Integer.parseInt(mesesStr);
+
+confirmarReserva(event, dataInicio, dataFim, meses);
+
+ popupStage.close(); // Fechar a mini tela ao confirmar
+
+ } catch (NumberFormatException ex) {
+
+ exibirAlertaErro("Erro", "Período inválido", "Por favor, insira um número válido de meses.");
+
+ }
+
+ } else {
+
+ exibirAlertaErro("Erro", "Dados inválidos", "Por favor, preencha todos os campos.");
+
+ }
+
+ });
+
+
+
+cancelarButton.setOnAction(e -> popupStage.close());
+
+ VBox layout = new VBox(10);
+
+ layout.getChildren().addAll(mesesTextField, dataInicioPicker, dataFimPicker, confirmarButton, cancelarButton);
+
+layout.setPadding(new Insets(20));
+
+
+ Scene scene = new Scene(layout);
+
+ popupStage.setScene(scene);
+
+ popupStage.showAndWait();
+
 }
 
 
 
 
-    private boolean reservarBem(LocalDate dataInicio, LocalDate dataFim, int meses) {
-        try {
-            LocalDateTime inicioPeriodo = dataInicio.atStartOfDay();
-            LocalDateTime fimPeriodo = dataFim.atStartOfDay();
 
-            List<String> periodosDisponiveisString = controladorReservas.consultarDisponibilidadeParaReserva(bem, inicioPeriodo, fimPeriodo, usuarioLogado);
 
-            List<LocalDate> datasDisponiveis = periodosDisponiveisString.stream()
-            .map(dateString -> LocalDateTime.parse(dateString, DateTimeFormatter.ISO_LOCAL_DATE_TIME)) 
-            .map(LocalDateTime::toLocalDate) 
-            .collect(Collectors.toList());
 
-            // Filtrar DatePickers para exibir apenas datas disponíveis
-            dataInicioPicker.setDayCellFactory(picker -> new DateCell() {
-                public void updateItem(LocalDate date, boolean empty) {
-                    super.updateItem(date, empty);
-                    if (empty || date == null || !datasDisponiveis.contains(date)) {
-                        setDisable(true);
-                        setStyle("-fx-background-color: #cccccc;"); // Cinza claro para datas indisponíveis
-                    } else {
-                        setStyle("-fx-background-color: #ccffcc;"); // Verde claro para datas disponíveis
-                    }
-                }
-            });
 
-            dataFimPicker.setDayCellFactory(picker -> new DateCell() {
-                public void updateItem(LocalDate date, boolean empty) {
-                    super.updateItem(date, empty);
-                    if (empty || date == null || !datasDisponiveis.contains(date)) {
-                        setDisable(true);
-                        setStyle("-fx-background-color: #cccccc;"); // Cinza claro para datas indisponíveis
-                    } else {
-                        setStyle("-fx-background-color: #ccffcc;"); // Verde claro para datas disponíveis
-                    }
-                }
-            });
 
-            // Verificar se o período selecionado está disponível
-            LocalDateTime dataAtual = inicioPeriodo;
-            boolean periodoDisponivel = true;
-            
-            while (dataAtual.isBefore(fimPeriodo)) {
-                if (!datasDisponiveis.contains(dataAtual.toLocalDate())) {
-                    periodoDisponivel = false;
-                    break;
-                }
-                dataAtual = dataAtual.plusDays(1);
-            }
-        return periodoDisponivel;
-    } catch (Exception e) {
-        exibirAlertaErro("Erro", "Erro ao verificar disponibilidade", e.getMessage());
-        return false;  
-    }
-    }
+private boolean reservarBem(LocalDate dataInicio, LocalDate dataFim, int meses) {
+
+try {
+
+ LocalDateTime inicioPeriodo = dataInicio.atStartOfDay();
+
+ LocalDateTime fimPeriodo = dataFim.atStartOfDay();
+
+
+
+ List<String> periodosDisponiveisString = controladorReservas.consultarDisponibilidadeParaReserva(bem, inicioPeriodo, fimPeriodo, usuarioLogado);
+
+
+
+List<LocalDate> datasDisponiveis = periodosDisponiveisString.stream()
+
+.map(dateString -> LocalDateTime.parse(dateString, DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+
+.map(LocalDateTime::toLocalDate)
+
+.collect(Collectors.toList());
+
+
+
+// Filtrar DatePickers para exibir apenas datas disponíveis
+
+dataInicioPicker.setDayCellFactory(picker -> new DateCell() {
+
+public void updateItem(LocalDate date, boolean empty) {
+
+ super.updateItem(date, empty);
+
+ if (empty || date == null || !datasDisponiveis.contains(date)) {
+
+ setDisable(true);
+
+ setStyle("-fx-background-color: #cccccc;"); // Cinza claro para datas indisponíveis
+
+ } else {
+
+setStyle("-fx-background-color: #ccffcc;"); // Verde claro para datas disponíveis
+}
+
+}
+
+ });
+
+ dataFimPicker.setDayCellFactory(picker -> new DateCell() {
+
+public void updateItem(LocalDate date, boolean empty) {
+
+super.updateItem(date, empty);
+if (empty || date == null || !datasDisponiveis.contains(date)) {
+
+ setDisable(true);
+
+ setStyle("-fx-background-color: #cccccc;"); // Cinza claro para datas indisponíveis
+
+} else {
+
+setStyle("-fx-background-color: #ccffcc;"); // Verde claro para datas disponíveis
+ }
+
+ }
+
+ });
+
+
+
+// Verificar se o período selecionado está disponível
+
+ LocalDateTime dataAtual = inicioPeriodo;
+
+ boolean periodoDisponivel = true;
+
+
+
+while (dataAtual.isBefore(fimPeriodo)) {
+
+ if (!datasDisponiveis.contains(dataAtual.toLocalDate())) {
+
+ periodoDisponivel = false;
+
+ break;
+
+ }
+
+ dataAtual = dataAtual.plusDays(1);
+
+ }
+
+ return periodoDisponivel;
+
+ } catch (Exception e) {
+
+ exibirAlertaErro("Erro", "Erro ao verificar disponibilidade", e.getMessage());
+
+ return false; 
+
+ }
+
+}
+
+
 
 public void confirmarReserva(ActionEvent event, LocalDate dataInicio, LocalDate dataFim, int meses) {
-    boolean periodoDisponivel = reservarBem(dataInicio, dataFim, meses);
-    
-    if (periodoDisponivel) {
-        LocalDateTime inicioPeriodo = dataInicio.atStartOfDay();
-        LocalDateTime fimPeriodo = dataFim.atStartOfDay();
-        
-        try {
-            controladorReservas.criarReserva(inicioPeriodo, fimPeriodo, usuarioLogado, bem);
-            exibirAlertaInfo("Sucesso", "Reserva realizada", "Reserva para " + bem.getNome() + " de " + dataInicio + " até " + dataFim + " realizada com sucesso!");
-        } catch (Exception e) {
-            exibirAlertaErro("Erro", "Erro ao criar reserva", e.getMessage());
-        }
-    } else {
-        exibirAlertaErro("Erro", "Período indisponível", "O período não está disponível para reserva.");
-    }
+
+ boolean periodoDisponivel = reservarBem(dataInicio, dataFim, meses);
+
+ if (periodoDisponivel) {
+
+ LocalDateTime inicioPeriodo = dataInicio.atStartOfDay();
+
+LocalDateTime fimPeriodo = dataFim.atStartOfDay();
+
+ try {
+
+ controladorReservas.criarReserva(inicioPeriodo, fimPeriodo, usuarioLogado, bem);
+
+ exibirAlertaInfo("Sucesso", "Reserva realizada", "Reserva para " + bem.getNome() + " de " + dataInicio + " até " + dataFim + " realizada com sucesso!");
+
+ } catch (Exception e) {
+
+exibirAlertaErro("Erro", "Erro ao criar reserva", e.getMessage());
+
+ }
+
+ } else {
+
+ exibirAlertaErro("Erro", "Período indisponível", "O período não está disponível para reserva.");
+
+}
+
 }
 
 
-    private void exibirAlertaErro(String titulo, String header, String contentText) {
-        Alert alerta = new Alert(Alert.AlertType.ERROR);
-        alerta.setTitle(titulo);
-        alerta.setHeaderText(header);
-        alerta.setContentText(contentText);
-        alerta.getDialogPane().setStyle("-fx-background-color: #ffcccc;"); // Vermelho claro
-        alerta.showAndWait();
-    }
 
-    private void exibirAlertaInfo(String titulo, String header, String contentText) {
-        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
-        alerta.setTitle(titulo);
-        alerta.setHeaderText(header);
-        alerta.setContentText(contentText);
-        alerta.showAndWait();
-    }
+
+
+ private void exibirAlertaErro(String titulo, String header, String contentText) {
+
+ Alert alerta = new Alert(Alert.AlertType.ERROR);
+
+ alerta.setTitle(titulo);
+
+ alerta.setHeaderText(header);
+
+ alerta.setContentText(contentText);
+
+ alerta.getDialogPane().setStyle("-fx-background-color: #ffcccc;"); // Vermelho claro
+
+ alerta.showAndWait();
+
+ }
+
+
+
+ private void exibirAlertaInfo(String titulo, String header, String contentText) {
+
+Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+
+alerta.setTitle(titulo);
+
+alerta.setHeaderText(header);
+
+ alerta.setContentText(contentText);
+
+ alerta.showAndWait();
+
+}
 }
--- src/main/java/br/ufrpe/timeshare/gui/controllers/usuarioComum/telaReservas/ControllerReservasComum.java
+++ src/main/java/br/ufrpe/timeshare/gui/controllers/usuarioComum/telaReservas/ControllerReservasComum.java
@@ -2,6 +2,7 @@ package br.ufrpe.timeshare.gui.controllers.usuarioComum.telaReservas;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ListCell;
@@ -19,6 +20,7 @@ import javafx.util.Callback;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -44,19 +46,15 @@ public class ControllerReservasComum {
     private ControladorBens controladorBens;
     private Usuario usuarioLogado;
 
-{
-    controladorBens = new ControladorBens();
-}
-
-    public void inicializar() {
-
+@FXML
+    public void initialize() {
+        this.controladorBens = new ControladorBens();
         // Usuário logado
         Object data = ScreenManager.getInstance().getData();
         if (data instanceof Usuario) {
             this.usuarioLogado = (Usuario) data;
         }
 
-        configurarListViews();
         exibirBensIniciais(); // Exibe todos os bens ao inicializar a tela
     }
 
@@ -70,42 +68,53 @@ public class ControllerReservasComum {
         alerta.showAndWait();
     }
 
-    private void configurarListViews() {
-        // Configuração para todosBensListView (bens ofertados)
+    private void configurarListViewTodosBens() {
         todosBensListView.setCellFactory(new Callback<>() {
             @Override
             public ListCell<Bem> call(ListView<Bem> listView) {
                 return new ListCell<>() {
+                    private FXMLLoader loader;
+                    private HBox root;
+                    private ControllerItemCellBemReserva controller;
+    
                     @Override
                     protected void updateItem(Bem item, boolean empty) {
                         super.updateItem(item, empty);
+    
                         if (empty || item == null) {
                             setGraphic(null);
                         } else {
-                            try {
-                                FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/ufrpe/timeshare/gui/controllers/celulas/ItemCellBemReserva.fxml"));
-                                HBox root = loader.load();
-                                ControllerItemCellBemReserva controller = loader.getController();
-                                controller.setItem(item);
-                                Button reservaButton = (Button) root.lookup("#reservaButton"); 
-                                reservaButton.setOnAction(event -> {
-                                    controller.abrirMiniTelaReserva(event);  
-                                });
-    
-                                setGraphic(root);
-                            } catch (IOException e) {
-                                exibirAlertaErro("Erro", "Erro ao exibir bens disponiveis", e.getMessage());
+                            if (loader == null) {
+                                try {
+                                    loader = new FXMLLoader(getClass().getResource("/br/ufrpe/timeshare/gui/application/ItemCellBemReserva.fxml"));
+                                    root = loader.load();
+                                    controller = loader.getController();
+                                } catch (IOException e) {
+                                    exibirAlertaErro("Erro", "Erro ao exibir bens disponíveis", e.getMessage());
+                                    return;
+                                }
                             }
+    
+                            // Atualiza os dados da célula
+                            controller.setItem(item);
+    
+                            // Configura ação do botão
+                            Button reservaButton = (Button) root.lookup("#reservaButton");
+                            if (reservaButton != null) {
+                                reservaButton.setOnAction(event -> controller.abrirMiniTelaReserva(event));
+                            }
+    
+                            setGraphic(root);
                         }
                     }
                 };
             }
         });
-    
-
+    }
     
 
         // Configuração para meusBensListView (bens com cotas)
+        private void configurarListViewMeusBens(){
         meusBensListView.setCellFactory(new Callback<>() {
             @Override
             public ListCell<Bem> call(ListView<Bem> listView) {
@@ -117,7 +126,7 @@ public class ControllerReservasComum {
                             setGraphic(null);
                         } else {
                             try {
-                                FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/ufrpe/timeshare/gui/controllers/celulas/ItemCellBemCota.fxml"));
+                                FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/ufrpe/timeshare/gui/application/ItemCellBemCota.fxml"));
                                 HBox root = loader.load();
                                 ControllerItemCellBemCota controller = loader.getController();
                                 controller.setItem(item);
@@ -131,6 +140,7 @@ public class ControllerReservasComum {
             }
         });
     }
+
 
     private void exibirBensIniciais() {
         buscarTodosBens();
@@ -151,6 +161,7 @@ public class ControllerReservasComum {
 
             ObservableList<Bem> items = FXCollections.observableArrayList(bensOfertados);
             todosBensListView.setItems(items);
+            configurarListViewTodosBens();
         } catch (Exception e) {
             exibirAlertaErro("Erro", "Erro ao tentar buscar bens", e.getMessage());
         }
@@ -170,9 +181,15 @@ public class ControllerReservasComum {
 
             ObservableList<Bem> items = FXCollections.observableArrayList(bensComCotas);
             meusBensListView.setItems(items);
+            configurarListViewMeusBens();
         } catch (Exception e) {
             exibirAlertaErro("Erro", "Erro ao tentar buscar bens", e.getMessage());
         }
+    }
+
+    @FXML
+    public void voltarTelaPrincipalComum(ActionEvent event){
+        ScreenManager.getInstance().showUsuarioComumPrincipalScreen();
     }
 
 }
